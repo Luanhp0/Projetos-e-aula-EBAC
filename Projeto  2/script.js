6b02b4a92ad8c7a2a0b6b6c6033b9684e24117a4
Luanhp0 <luanpereirahp0@gmail.com>
2023-12-18 16:47:49 -0300

    Terminando o espacamento da coluna

--- Projeto  2/script.js
+++ Projeto  2/script.js
@@ -1,17 +1,24 @@
 const form = document.getElementById('formulario');
+let linhas = '';
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     const inputNome = document.getElementById('nome');
     const inputNumero = document.getElementById('numero');
 
+
+    
+
     let linha = '<tr>';
     linha += `<td>${inputNome.value}</td>`;
     linha += `<td>${inputNumero.value}</td>`;
     linha += `</tr>`;
 
+    linhas += linha;
+
     const corpotabela = document.querySelector('tbody');
-    corpotabela.innerHTML = linha;
+    corpotabela.innerHTML = linhas;
     
 
 
